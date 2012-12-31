5c07cec21a289d844b7ade76e17a652ce40b2f67
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-12-31 09:15:24 +0900

    change hosting url

--- lib/yabitz/misc/errors.rb
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-module Yabitz
-  class DuplicationError < StandardError;  end
-  class InconsistentDataError < StandardError; end
-end
--- lib/yabitz/model.rb
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-require_relative 'model/auth_info'
-require_relative 'model/addresses'
-require_relative 'model/company'
-require_relative 'model/informations'
-require_relative 'model/host_misc'
-require_relative 'model/host'
-require_relative 'model/brick'
